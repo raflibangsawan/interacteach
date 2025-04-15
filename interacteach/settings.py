b98e96c4943e712a64c5848dc6b0859f074b30f9
Rafli Bangsawan <raflibangsawan@Raflis-MacBook-Pro.local>
2025-04-15 22:34:16 +0700

    set debug to false

--- interacteach/settings.py
+++ interacteach/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'interacteach-production.up.railway.app',
